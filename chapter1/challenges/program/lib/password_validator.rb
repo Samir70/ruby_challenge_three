1e692a0e7cb8ec95a3b1ee6a2927c972e4963ca8
Samir70 <sbetmouni@talktalk.net>
2022-10-25 15:19:38 +0100

    add code for password validator

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -1,6 +1,13 @@
 # Valid passowords are
-# - 8 or more characters
-# - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    # # - 8 or more characters
+    # if password.length < 8
+    #     return false
+    # end
+    # # - include at least one of these characters: !, @, $, %, &
+    # if password.count('!@$%&') == 0
+    #     return false
+    # end
+    return password.length > 7 && password.count('!@$%&') > 0
 end
